f92624f846b8b124b31cad237302214261903d15
Romie Hecdivert <leto@Romies-MacBook-Pro.local>
2020-07-29 07:23:52 -0400

    Added two more API routes - to update the workout and to get the workouts. Console logging the data to make sure that I am on the right track.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -30,7 +30,32 @@ app.post("api/workouts", ({ body }, res) => {
 
 
 // The GET route for "/api/workouts/range"
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+        console.log(dbWorkout);
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
 
 
 // Need to be able to find the workout and update it.
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+        { id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+    
+
 }
