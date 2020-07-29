2be774735495cebc3ea585d8b7c858156deb6a17
Romie Hecdivert <leto@Romies-MacBook-Pro.local>
2020-07-29 14:04:04 -0400

    Corrected some syntax issues, but still have errors when I try to save workouts.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,4 @@
-const Workout = ("../models/workout");
+const Workout = ("../models/Workout");
 
 module.exports = (app) => {
 // Referencing the api.js file, the following API routes are needed:
@@ -17,10 +17,9 @@ app.get("api/workouts", (req, res) => {
 // The POST route for "/api/workouts/" to create the workout.
 app.post("api/workouts", (req, res) => {
     Workout.create({})
-    .then((dbWorkout => {
-        console.log(dbWorkout);
-        res.json(dbWorkout);
-    })).catch(err => {
+    .then(dbWorkout =>
+        res.json(dbWorkout))
+    .catch(err => {
         res.json(err);
     });
 });
@@ -48,12 +47,12 @@ app.post("/api/workouts/range", (req, res) => {
 });
 
 // Need to be able to find the workout and update it.
-app.put("/api/workouts/:id", ({body, params}, res) => {
+app.put("/api/workouts/:id", (req, res) => {
     Workout.findOneAndUpdate(
-        { id: req.params.id },
+        { _id: req.params.id },
         {
             $inc: { totalDuration: req.body.duration },
-            $push: { exercises: body }
+            $push: { exercises: req.body }
         },
         { new: true })
         .then(dbWorkout => {
@@ -62,5 +61,4 @@ app.put("/api/workouts/:id", ({body, params}, res) => {
             res.json(err);
         });
     });
-    
 }
