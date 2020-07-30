bf2b2c89891ed5666f8f7cb0f2ad54c83d45e15e
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-07-29 20:49:30 -0400

    With much help, I was able to figure out a way to add up the sum of the durations for the exercises. I ran the application and all was successful.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,8 @@
 const Workout = require("../models/Workout.js");
-Workout.collection.drop();
+// Workout.collection.drop();
 
 module.exports = (app) => {
-// The GET route for "/api/workouts/"
+// The GET route for all of the workouts.
 app.get("/api/workouts", (req, res) => {
     Workout.find({}, (err, dbWorkout) => {
         if(err) {
@@ -14,7 +14,7 @@ app.get("/api/workouts", (req, res) => {
     });
 });
 
-// The POST route for "/api/workouts/" to create the workout.
+// The POST route to create the workout.
 app.post("/api/workouts", (req, res) => {
     Workout.create({}, (err, dbWorkout) => {
         if(err) {
@@ -26,7 +26,7 @@ app.post("/api/workouts", (req, res) => {
     });
 });
 
-// The GET route for "/api/workouts/range"
+// The GET route.
 app.get("/api/workouts/range", (req, res) => {
     Workout.find({}, (err, dbWorkout) => {
         if(err) {
@@ -38,6 +38,7 @@ app.get("/api/workouts/range", (req, res) => {
     });
 });
 
+// The POST route.
 app.post("/api/workouts/range", (req, res) => {
     Workout.create({})
     .then(dbWorkout => {
@@ -48,14 +49,15 @@ app.post("/api/workouts/range", (req, res) => {
     });
 });
 
-// Need to be able to find the last workout and update it.
+// Route to "find" the last workout and update it. The route also calculates the total duration of the exercises of a workout.
 app.put("/api/workouts/:id", (req, res) => {
     Workout.findOneAndUpdate(
         { _id: req.params.id },
-        { $push: { exercises: req.body } },
+        { $push: { exercises: req.body }, $inc: { totalDuration: req.body.duration } },
         { new: true, runValidators: true})
-        .populate("exercises")
         .then(dbWorkout => {
+            console.log(req.body);
+            console.log(req.body.duration);
             res.json(dbWorkout);
         }).catch(err => {
             res.json(err);
